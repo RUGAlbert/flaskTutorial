4c21a789dc88b3772d68f4be609a50bd1b9e7309
Albert <a.dijkstra.38@student.rug.nl>
2019-10-07 08:54:51 +0200

    changed some file structues

--- app/githubModule.py/user.py
+++
@@ -1,6 +0,0 @@
-
-
-class User:
-	def __init__(self, name):
-		self.name = self.name
-https://api.github.com/search/users?q=
--- config.py
+++
@@ -1,5 +0,0 @@
-import os
-
-class Config(object):
-    VERSION = '0.1.0'
-    ISDEBUG = (os.environ.get('ISDEBUG', 'True') == 'True')
